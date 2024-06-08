599c2008ce7469025f98f706291c3cb59ed7ad8b
sohyeon Park <qkrthgus7026.gmail.com>
2024-06-08 11:29:45 +0900

    mariadb사용을 위한 의존성 설정

--- Egov_WEB/src/main/java/egov/main/web/MainController.java
+++ Egov_WEB/src/main/java/egov/main/web/MainController.java
@@ -4,13 +4,48 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-	@RequestMapping(value="main.do")
+	@RequestMapping(value="/main.do")
 	public String main(HttpServletRequest request, ModelMap model)
 	{
 		return "main/main";
+	}
+	
+	@RequestMapping(value="/main2.do")
+	public String main2(HttpServletRequest request, ModelMap model)
+	{
+		return "main/main2";
+	}
+	
+	@RequestMapping(value="/main3.do")
+	public String main3(@RequestParam("pw")String pw, HttpServletRequest request, ModelMap model)
+	{
+		String id = request.getParameter("id");
+		int userNo = Integer.parseInt(request.getParameter("userNo"));
+		
+		if(id.equals("qwer"))
+		{
+			model.addAttribute("userid", pw);
+		}
+		else 
+		{
+			model.addAttribute("userid", pw);
+		}
+		
+		userNo = userNo + 5;
+		model.addAttribute("userNo", userNo);
+		return "main/main3";
+	}
+	
+	@RequestMapping(value="/main4/{userNo}.do")
+	public String main4(@PathVariable String userNo, HttpServletRequest request, ModelMap model)
+	{
+		model.addAttribute("userNo", userNo);
+		return "main/main3";
 	}
 }
